186e7f41a9947837fa2584784aba2356305d32b1
g4n3sha5 <kmatysiak-it@outlook.com>
2024-02-07 15:03:01 +0100

    quick-fix

--- components/common/ImageBanner.js
+++ components/common/ImageBanner.js
@@ -1,9 +1,9 @@
 import Image from 'next/image';
 export const ImageBanner = ({ text }) => {
   return (
-    <div className="imageBanner d-flex align-items-center justify-content-center pt-5 text-center w-100 h-100">
-      {/* <Image src="img/posnaniafyrtel.jpg" layout="fill" className=" object-fit-cover" /> */}
-      {/* <div className="bannerOverlay"></div> */}
+    <div className="imageBanner d-flex align-items-center justify-content-center pt-5 overflow-hidden text-center w-100 ">
+      {/* <Image src="/img/posnaniafyrtel.jpg" layout="fill" className=" object-fit-cover" />
+      <div className="bannerOverlay"></div> */}
       <h1>{text}</h1>
     </div>
   );
