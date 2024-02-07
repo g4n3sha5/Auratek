0d4e5a7090909018cbb5dd2e41b5d8973cba18a9
g4n3sha5 <kmatysiak-it@outlook.com>
2024-02-07 15:39:49 +0100

    image-priority

--- components/common/ImageBanner.jsx
+++ components/common/ImageBanner.jsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export const ImageBanner = ({ text, src = '/img/anastasiya-dalenka-Warszawa.jpg' }) => {
   return (
     <div className="imageBanner d-flex align-items-center position-relative text-white justify-content-center pt-5 overflow-hidden text-center w-100 ">
-      <Image src={src} layout="fill" className="object-fit-cover" />
+      <Image src={src} priority={true} layout="fill" className="object-fit-cover" />
       <div className="bannerOverlay"></div>
       <h1>{text}</h1>
     </div>
